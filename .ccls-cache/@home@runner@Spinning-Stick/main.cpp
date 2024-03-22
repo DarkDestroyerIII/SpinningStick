39336998c264ef05bd5eff10811dac7b3176bf51
DarkDestroyerIII <yoozernam3@gmail.com>
2024-03-22 01:11:15 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Spinning-Stick/main.cpp
@@ -0,0 +1,35 @@
+#include<thread>
+#include<iostream>
+#include<mutex>
+#include<future>
+#include<chrono>
+
+using namespace std;
+int main(){
+  cout << "I wonder If I'll get deleted..." << endl;
+
+  auto inputThread = async(launch::async,[]() {
+    while (cin.get() != '\n') {};
+   //cin.get();
+    });
+
+  while (inputThread.wait_for(0ms)!= future_status::ready) {
+    char symbols[] = { '-','\\','|','/' };
+    for (int i = 0; i < 4; ++i) {
+        if (inputThread.wait_for(50ms) == future_status::ready) break;
+
+        //Making the first \r separate makes the output not freak out as much for some reason
+        //cout << "\r\x1B[?25l\x1B[92m Press Enter to Continue... " << symbols[i] <<  "\x1B[K\r\x1B[?25l";// added escape sequence to clear everything after the cursor
+        cout << "\r\x1B[?25l\x1B[92mPress Enter to Continue... " << symbols[i] << "\x1B[K";
+
+        this_thread::sleep_for(chrono::milliseconds(50));//              The A is cursor up
+        cout << "\x1B[97m\x1B[?25h";//moves cursure up 1 and sets color white
+    }
+  }
+  cout << "Stall successfully ended";
+  /// cin.ignore(255,'\n');
+  string test;
+  getline(cin, test);
+
+  cout << test.size() << " characters left in the stream.\n";
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Spinning wait
+
+This is a simple program that can be used to wait for user input while displaying a message.
+
+<details>
+  <summary>Facts/Features</summary>
+
+This program features multiple escape sequences which clean up the terminal so that it can display. Currently it is capable of displaying the animation in a very smooth manner, and doesn't become distorted through user interference.
+
+</details>
